b1022542a43044cd28a2016db26a6d1bc9ce37cd
JJTech0130 <jjtech@jjtech.dev>
2022-12-26 17:52:45 -0500

    clean get_symbol

--- src/android_library.rs
+++ src/android_library.rs
@@ -2,26 +2,20 @@ use memmap2::MmapMut;
 
 pub(crate) struct Symbol {
     pub(crate) name: String,
-    pub(crate) value: usize
+    pub(crate) value: usize,
 }
 
 pub struct AndroidLibrary {
     pub(crate) memory_map: MmapMut,
-    pub(crate) symbols: Vec<Symbol>
+    pub(crate) symbols: Vec<Symbol>,
 }
 
 impl AndroidLibrary {
-    pub fn get_symbol(&self, symbol_name: &str) -> Option<extern "C" fn()> {
-        // we don't use Elf hashes, because I'm to lazy to figure out how to get the functions from
-        // hashes and memory_map without triggering the borrow checker.
-        let mut symbol_value = None;
-
-        for symbol in &self.symbols {
-            if symbol.name == symbol_name {
-                symbol_value = Some(symbol.value);
-            }
-        }
-
-        symbol_value.map(|val| unsafe { std::mem::transmute(self.memory_map.as_ptr() as usize + val) })
+    pub fn get_symbol(&self, symbol_name: &str) -> Option<*const ()> {
+        // the typically way to do this uses hashes, but this works fine if not maximally efficient
+        self.symbols
+            .iter()
+            .find(|s| s.name == symbol_name)
+            .map(|s| unsafe { self.memory_map.as_ptr().offset(s.value as isize) as *const () })
     }
 }
